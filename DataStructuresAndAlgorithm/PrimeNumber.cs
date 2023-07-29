2c414b8945e9f0d5ed2eb05cc55dc10efa68554a
Soundarya <67475352+soundarya12s@users.noreply.github.com>
2023-07-29 17:54:19 +0530

    Update PrimeNumber.cs

--- DataStructuresAndAlgorithm/PrimeNumber.cs
+++ DataStructuresAndAlgorithm/PrimeNumber.cs
@@ -11,22 +11,24 @@ namespace DataStructuresAndAlgorithm
         public void IsPrimeNumber(int number)
         {
             bool IsPrime = true;
-            for (int i = 2; i < number / 2; i++)
+            Console.WriteLine("Prime Numbers upto 1000 : ");
+            for (int i = 2; i <= 100; i++)
             {
-                if (number % i == 0)
+                for (int j = 2; j <= 100; j++)
                 {
-                    IsPrime = false;
-                    break;
+ 
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+ 
                 }
-            }
-            if (IsPrime)
-            {
-                Console.Write("Number is Prime.");
-            }
-            else
-            {
-                Console.Write("Number is not Prime.");
-            }
+                if (isPrime)
+                {
+                    Console.Write("\t" +i);
+                }
+                isPrime = true;
         }
     }
 }
